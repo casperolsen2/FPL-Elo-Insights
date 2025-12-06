2d35bdb35c3da4b506abb986888bdb96d4ed7a94
casperolsen2 <casperolsen110@gmail.com>
2025-12-06 21:38:12 +0100

    lagt til funksjoner

--- .github/workflows/Eget script/Funksjoner.py
+++ .github/workflows/Eget script/Funksjoner.py
@@ -1,30 +1,43 @@
 import pandas as pd
 from pathlib import Path
+from Lese_Nyeste_GW import siste_stats_per_spiller
 
-def hent_aktuell_spillerdata(base_path):
-    base = Path(base_path)
-    
-    # 1. Finn siste ferdige gameweek
-    gw_summaries = pd.read_csv(base / "gameweek_summaries.csv")
-    ferdige = gw_summaries[gw_summaries["finished"] == True]
-    siste_gw_id = int(ferdige["id"].max())
-    
-    # 2. Les playerstats fra riktig GW-mappe
-    gw_mappe = base / "By Gameweek" / f"GW{siste_gw_id}"
-    df = pd.read_csv(gw_mappe / "playerstats.csv")
-    
-    return df, siste_gw_id
 
-BASE_PATH = r"/Users/casperolsen/Documents/GitHub/FPL-Elo-Insights/data/2025-2026"
-df, siste_gw = hent_aktuell_spillerdata(BASE_PATH)
+ps = pd.read_csv("data/2025-2026/playerstats.csv")
+ps_clean = siste_stats_per_spiller(ps)
 
-def høyest_ppg(df):
-    _, ppg = next(df.iterrows())
-    ppg = df.iloc[0]
-    for _, spiller in df.iterrows():
+def høyest_ppg(ps_clean):
+    _, ppg = next(ps_clean.iterrows())
+    ppg = ps_clean.iloc[0]
+    for _, spiller in ps_clean.iterrows():
         if float(spiller['points_per_game']) > float(ppg['points_per_game']):
             ppg = spiller
     return ppg
 
-ppg = høyest_ppg(df)
-print(ppg["first_name"], ppg["points_per_game"])
+def dyreste_spiller(ps_clean):
+    _, dyreste = next(ps_clean.iterrows())
+    dyreste = ps_clean.iloc[0]
+    for _, spiller in ps_clean.iterrows():
+        if spiller['now_cost'] > dyreste['now_cost']:
+            dyreste = spiller
+    return dyreste
+
+def moneyball(ps_clean):
+    _, moneyball = next(ps_clean.iterrows())
+    moneyball = ps_clean.iloc[0]
+    for _, spiller in ps_clean.iterrows():
+        if spiller['total_points'] == 0:
+            continue
+        if spiller['total_points'] / spiller['now_cost'] > moneyball['total_points'] / moneyball['now_cost']:
+            moneyball = spiller
+    return moneyball
+
+print(moneyball(ps_clean)['second_name'], moneyball(ps_clean)['first_name'])
+
+def moneyball_list(ps_clean):
+    liste = ps_clean[(ps_clean['total_points'] > 0) & (ps_clean["now_cost"] > 0)].copy()
+    liste['value'] = liste['total_points'] / liste['now_cost']
+
+    topp10 = liste.sort_values("value", ascending = False).head(10)
+    return topp10
+
